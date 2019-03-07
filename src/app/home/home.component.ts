e7e48018ea9cbf48c680f9c601f07d13bc29d9ab
nyIdeas <niyasim.p@gmail.com>
2019-03-07 12:51:31 +0400

    my first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
+import { DataService } from '../data.service';
+ 
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [ trigger('goals', [
+    transition('* => *', [
+
+      query(':enter', style({ opacity: 0 }), {optional: true}),
+
+      query(':enter', stagger('300ms', [
+        animate('.6s ease-in', keyframes([
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+        ]))]), {optional: true}),
+      query(':leave', stagger('300ms', [
+        animate('.6s ease-in', keyframes([
+          style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+          style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+        ]))]), {optional: true})
+    ])
+  ])
+
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  itemCount:number;
+  btnTxt:string='Add an Item';
+  lifeGoal:string='Life goal...';
+  goalStr:string='My First Life Goal';
+  goals=[];
+  constructor(private _data:DataService) { 
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
+  }
+
+  ngOnInit() {
+    this.itemCount=this.goals.length;
+  }
+  addItem(){
+    this.goals.push(this.goalStr);
+    this.goalStr='';
+    this._data.changeGoal(this.goals);
+    this.itemCount=this.goals.length;
+  }
+  removeItem(i){
+    this.goals.splice(i,1);
+    this._data.changeGoal(this.goals);
+    this.itemCount=this.goals.length;
+  }
+}
